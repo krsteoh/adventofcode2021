e7eb6327cc7dc3537bc69407bb0b4663908b3ef5
Krste Iloski <Krste.Iloski@btobet.com>
2021-12-16 22:23:27 +0100

    a

---
+++ Day15/Task15.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day15
+{
+    public static class Task15
+    {
+
+        const int day = 15;
+        static string Day = string.Format("Day{0}", day);
+        static string FilePath = string.Format(@"Day{0}\d{0}.txt", day);
+        static string TestFilePath = string.Format(@"Day{0}\d{0}test.txt", day);
+
+        public static void Start()
+        {
+             GlobalFunctions.ConsolePrintTask(string.Format("{0}- Task1 Test Result = ", Day), DoTask1Test);
+           // GlobalFunctions.ConsolePrintTask(string.Format("{0}- Task1 Result = ", Day), DoTask1);
+            //GlobalFunctions.ConsolePrintTask(string.Format("{0}- Task1 Test Result = ", Day), DoTask2Test);
+            //GlobalFunctions.ConsolePrintTask(string.Format("{0}- Task2 Result = ", Day), DoTask2);
+        }
+        public static string DoTask2(List<string> l)
+        {
+     
+            return null;
+        }
+
+        public static string DoTask1(List<string> l)
+        {
+            var matrix = CreateMatrix(l);
+            DrawMatrix(matrix);
+            return null;
+        }
+        public static string DoTask1Test()
+        {
+            return DoTask1(System.IO.File.ReadAllLines(string.Format("{0}{1}", Config.LocalPath, TestFilePath)).ToList());
+        }
+        public static string DoTask1()
+        {
+            return DoTask1(System.IO.File.ReadAllLines(string.Format("{0}{1}", Config.LocalPath, FilePath)).ToList());
+        }
+        public static string DoTask2Test()
+        {
+            return DoTask2(System.IO.File.ReadAllLines(string.Format("{0}{1}", Config.LocalPath, TestFilePath)).ToList());
+        }
+
+        public static string DoTask2()
+        {
+            return DoTask2(System.IO.File.ReadAllLines(string.Format("{0}{1}", Config.LocalPath, FilePath)).ToList());
+        }
+
+        private static int[,] CreateMatrix(List<string> list)
+        {
+            int rows = list.Count;
+            int cols = list[0].Trim().Length;
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var c = list[i].Trim().ToArray().Select(t => int.Parse(t.ToString())).ToArray();
+                for (int j = 0; j < cols; j++) matrix[i, j] = c[j];
+            }
+            return matrix;
+        }
+
+        private static void DrawMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                string str = "";
+                for (int j = 0; j < cols; j++)
+                    str += matrix[i, j];
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using AdventOfCode.Day11;
 using AdventOfCode.Day12;
 using AdventOfCode.Day13;
 using AdventOfCode.Day14;
+using AdventOfCode.Day15;
 using AdventOfCode.Day2;
 using AdventOfCode.Day3;
 using AdventOfCode.Day4;
@@ -34,7 +35,8 @@ namespace AdventOfCode
             //Task11.Start();
             //Task12.Start();
             //Task13.Start();
-            Task14.Start();
+            //Task14.Start();
+            Task15.Start();
         }
     }
 }
